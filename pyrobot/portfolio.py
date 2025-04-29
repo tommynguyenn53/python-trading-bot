3bba7c2cc7b50ec71f1799ad9f0a20842a0b9afc
Tommy Nguyen <tong9587@uni.sydney.edu.autong9587@uni.sydney.edu.autong9587@uni.sydney.edu.au>
2025-04-29 19:01:29 +1000

    Added trade configuration logic and technical indicator methods (RSI, SMA, EMA) with dynamic signal evaluation support

--- pyrobot/portfolio.py
+++ pyrobot/portfolio.py
@@ -4,6 +4,7 @@ from typing import Union
 from typing import Optional
 from typing import Tuple
 
+
 class Portfolio():
 
     def __init__(self, account_number: Optional[str]):
@@ -15,7 +16,8 @@ class Portfolio():
         self.risk_tolerance = 0.0
         self.account_number = account_number
 
-    def add_position(self, symbol: str, asset_type: str, purchase_date: Optional[str], quantity: int = 0, purchase_price: float = 0.0) -> dict:
+    def add_position(self, symbol: str, asset_type: str, purchase_date: Optional[str], quantity: int = 0,
+                     purchase_price: float = 0.0) -> dict:
 
         self.positions[symbol] = {}
         self.positions[symbol]['symbol'] = symbol
@@ -30,7 +32,6 @@ class Portfolio():
 
         if isinstance(positions, list):
             for position in positions:
-
                 self.add_position(
                     symbol=position['symbol'],
                     asset_type=position['asset_type'],
@@ -44,16 +45,15 @@ class Portfolio():
         else:
             raise TypeError('positions must be a list of dictionaries')
 
-
     def remove_position(self, symbol: str) -> Tuple[bool, str]:
 
         if symbol in self.positions:
             del self.positions[symbol]
 
-            return (True, "{Symbol} was sucessfully removed".format(Symbol=symbol))
+            return True, "{Symbol} was successfully removed".format(Symbol=symbol)
 
         else:
-            return (False, "{Symbol} did not exist in the portfolio".format(Symbol=symbol))
+            return False, "{Symbol} did not exist in the portfolio".format(Symbol=symbol)
 
     def in_portfolio(self, symbol: str) -> bool:
 
@@ -61,16 +61,16 @@ class Portfolio():
             return True
 
         else:
-                return False
+            return False
 
     def is_profitable(self, symbol: str, current_price: float) -> bool:
 
         purchase_price = self.positions[symbol]['purchase_price']
 
-        if (purchase_price =< current_price):
+        if purchase_price <= current_price:
             return True
 
-        elif (purchase_price > current_price):
+        elif purchase_price > current_price:
             return False
 
     def total_allocation(self):
@@ -78,4 +78,3 @@ class Portfolio():
 
     def risk_exposure(self):
         pass
-
